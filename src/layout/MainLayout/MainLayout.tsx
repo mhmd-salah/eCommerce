b473298bb6327a291c0a674a0980dbd3c360c488
mohamed salah <ms.mhmdsalah@gmail.com>
2024-12-08 15:24:49 +0200

    header|done

---
+++ src/layout/MainLayout/MainLayout.tsx
@@ -0,0 +1,17 @@
+import { Container} from "react-bootstrap"
+import styles from "./styles.module.css"
+import Header from "../../components/common/Header/Header";
+
+const { container, wrapper } = styles;
+const MainLayout = () => {
+  return (
+    <Container className={container}>
+      <Header/>
+      <div className={wrapper}>
+
+      </div>
+    </Container>
+  )
+}
+
+export default MainLayout
