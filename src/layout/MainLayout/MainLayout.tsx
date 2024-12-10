cf77cb40a1f9e172eb5a7ac94fd39d5218bb0211
mohamed salah <ms.mhmdsalah@gmail.com>
2024-12-10 23:48:53 +0200

    routes|done

--- src/layout/MainLayout/MainLayout.tsx
+++ src/layout/MainLayout/MainLayout.tsx
@@ -1,6 +1,7 @@
 import { Container } from "react-bootstrap";
 import styles from "./styles.module.css";
-import { Footer, Header } from "../../components/common";
+import { Footer, Header } from "@components/common";
+import { Outlet } from "react-router-dom";
 
 const { container, wrapper } = styles;
 const MainLayout = () => {
@@ -8,7 +9,7 @@ const MainLayout = () => {
     <Container className={container}>
       <Header />
       <div className={wrapper}>
-        
+        <Outlet/>
       </div>
       <Footer />
     </Container>
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,54 @@
-import { createRoot } from 'react-dom/client'
-import MainLayout from './layout/MainLayout/MainLayout';
-import "bootstrap/dist/css/bootstrap.min.css"
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+//layout
+import MainLayout from "@layout/MainLayout/MainLayout";
+//pages
+import Home from "@pages/Home";
+import Products from "@pages/Products";
+import Categories from "@pages/Categories";
+import AboutUs from "@pages/AboutUs";
 
-createRoot(document.getElementById('root')!).render(<MainLayout/>)
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <MainLayout />,
+//     children: [
+//       {
+//         index: true,
+//         element: <Home />,
+//       },
+//       {
+//         path: "categories",
+//         element: <Categories />,
+//       },
+//       {
+//         path: "products:prefix",
+//         element: <Products />,
+//       },
+//       {
+//         path: "about-us",
+//         element: <AboutUs />,
+//       },
+//     ],
+//   },
+// ]);
+const routes = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout/>}>
+      <Route index element={<Home />} />
+      <Route path="categories" element={<Categories />} />
+      <Route path="products/:prefix" element={<Products />} />
+      <Route path="about-us" element={<AboutUs />} />
+    </Route>
+  )
+)
+
+createRoot(document.getElementById("root")!).render(
+  <RouterProvider router={routes} />
+);
