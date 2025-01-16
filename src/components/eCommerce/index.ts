cbd1d3d9580da87d8c87ecdc4bbdcee22199fa92
mohamed salah <ms.mhmdsalah@gmail.com>
2025-01-16 20:27:38 +0200

    snapshot

--- src/components/eCommerce/index.ts
+++ src/components/eCommerce/index.ts
@@ -1,3 +1,2 @@
-export {default as HeaderBasket} from "./HeaderBasket/HeaderBasket";
-import {default as Category} from "./Category/Category";
-import {default as Product} from "./Product/Product";
+export { default as HeaderBasket } from "./HeaderBasket/HeaderBasket";
+export { default as CartItem } from "./CartItem/CartItem";
--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,9 +1,13 @@
 import Heading from "@components/common/Heading/Heading";
+import { CartItem } from "@components/eCommerce";
 
 const Cart = () => {
   return (
     <div>
       <Heading>Cart</Heading>
+      <CartItem/>
+      <CartItem/>
+      <CartItem/>
     </div>
   )
 }
