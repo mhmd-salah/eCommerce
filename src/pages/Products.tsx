79ae4019e82dceb6e2907a8ab793b15843f7ddad
mohamed salah <ms.mhmdsalah@gmail.com>
2025-05-23 02:11:23 +0200

    create category and product component

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,5 +1,28 @@
+import Product from "@components/eCommerce/Product.tsx/Product";
+import { Col, Container, Row } from "react-bootstrap";
+
 const Products = () => {
-  return <div>products</div>;
+  return (
+    <Container>
+      <Row>
+        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
+          <Product />
+        </Col>
+        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
+          <Product />
+        </Col>
+        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
+          <Product />
+        </Col>
+        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
+          <Product />
+        </Col>
+        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
+          <Product />
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Products;
