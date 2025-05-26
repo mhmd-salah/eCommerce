fa6f1a256ec7289f3d37f0324e5ff26bf820608e
mohamed salah <ms.mhmdsalah@gmail.com>
2025-05-26 22:06:46 +0200

    pending

--- src/store/categories/categoriesSlice.ts
+++ src/store/categories/categoriesSlice.ts
@@ -1,6 +1,12 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
+interface ICategoriesState {
+  records: { id: number; title: string; prefix: string; img: string }[];
+  loading: "idle" | "pending" | "success" | "failed";
+  error: string | null;
+}
+
+const initialState: ICategoriesState = {
   records: [],
   loading: "idle",
   error: null,
